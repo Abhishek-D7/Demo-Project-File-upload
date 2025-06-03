da4eb2e5a8b2e178ec6008d7c1c35e924d330d66
Abhishek Dhaka <abhi.dhaka1016@gmail.com>
2025-06-03 14:31:29 +0530

    File Uploading using FastAPI

---
+++ main.py
@@ -0,0 +1,12 @@
+import shutil
+from fastapi import FastAPI, UploadFile
+app = FastAPI()
+
+@app.post("/upload")
+async def upload_file(uploaded_file: UploadFile):
+    print(uploaded_file.file)
+    print(uploaded_file.filename)
+    print(uploaded_file.content_type)
+    with open(uploaded_file.filename, "wb") as buffer:
+        shutil.copyfileobj(uploaded_file.file, buffer)
+    return {"filename": uploaded_file.filename, "message": "Saved with stream method"}
